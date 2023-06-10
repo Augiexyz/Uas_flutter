f8dbaba079383391900a1be2404488354fb9324f
KhanisaOctavia <133880596+KhanisaOctavia@users.noreply.github.com>
2023-06-10 19:03:45 +0800

    Update CartPage.dart

--- lib/Pages/CartPage.dart
+++ lib/Pages/CartPage.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:food_order/widgets/CartBottomNavBar.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+  const CartPage({super.key}); 
 
   @override
   Widget build(BuildContext context) {

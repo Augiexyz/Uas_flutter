80b759be10f6b6568ea5c10737da8c3d5caa1649
Hardheana <133770678+Hardheana@users.noreply.github.com>
2023-06-10 19:06:38 +0800

    Update HomePage.dart

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -10,6 +10,8 @@ import 'package:food_order/widgets/CategoriesWidget.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
